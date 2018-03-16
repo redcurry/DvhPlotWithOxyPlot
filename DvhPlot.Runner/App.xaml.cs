8a622fac4765657b8dd9503d5d99350a866013b6
Carlos Anderson <carlosjanderson@gmail.com>
2018-03-16 19:51:40 -0400

    Use EclipsePlugInRunner to make debugging easier

---
+++ DvhPlot.Runner/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using EclipsePlugInRunner.Scripting;
+
+namespace DvhPlot.Runner
+{
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            ScriptRunner.Run(new VMS.TPS.Script());
+        }
+    }
+}
--- DvhPlot.Script/Script.cs
+++ DvhPlot.Script/Script.cs
@@ -1,13 +1,35 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using DvhPlot.Script;
 using VMS.TPS.Common.Model.API;
 
-namespace DvhPlot.Script
+namespace VMS.TPS
 {
     public class Script
     {
         public void Execute(ScriptContext context, Window window)
         {
-            var mainViewModel = new MainViewModel(context.PlanSetup);
+            Run(context.CurrentUser,
+                context.Patient,
+                context.Image,
+                context.StructureSet,
+                context.PlanSetup,
+                context.PlansInScope,
+                context.PlanSumsInScope,
+                window);
+        }
+
+        public void Run(
+            User user,
+            Patient patient,
+            Image image,
+            StructureSet structureSet,
+            PlanSetup planSetup,
+            IEnumerable<PlanSetup> planSetupsInScope,
+            IEnumerable<PlanSum> planSumsInScope,
+            Window window)
+        {
+            var mainViewModel = new MainViewModel(planSetup);
             var mainView = new MainView(mainViewModel);
             window.Content = mainView;
         }
