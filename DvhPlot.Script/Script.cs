f631da8f693aa5713e1acd9dac710e27cb6b1e81
Carlos Anderson <carlosjanderson@gmail.com>
2018-03-16 19:15:35 -0400

    Create new binary plug-in solution

---
+++ DvhPlot.Script/Script.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using VMS.TPS.Common.Model.API;
+
+namespace DvhPlot.Script
+{
+    public class Script
+    {
+        public void Execute(ScriptContext context, Window window)
+        {
+        }
+    }
+}
